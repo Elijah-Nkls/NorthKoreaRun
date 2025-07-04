277be6bc76a3c28edada7291050eade1699e25e9
Runsheng Song <runsheng.s@gso.schule.koeln>
2025-07-04 03:00:08 +0200

    Ghost-Kollision handhaben und Level implementieren Restart / Mehrere Geister als Flaggen zeichnen

--- NorthKoreaRun/Form1.cs
+++ NorthKoreaRun/Form1.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Drawing;
 using System.Collections.Generic;
-using System.Windows.Forms;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace Northkorea_Run
 {
     public partial class Form1 : Form
     {
-        private System.Windows.Forms.Timer gameTimer;
+        private Timer gameTimer;
         private List<Rectangle> walls;
         private List<Point> points;
         private Rectangle exitDoor;
@@ -19,17 +19,11 @@ namespace Northkorea_Run
         private int playerSpeed = 8;
         private int playerDx, playerDy;
         private int wantedDx, wantedDy;
-
-        public Form1()
+        private int currentLevel;
+        private bool[] levelCompleted = new bool[3];
+        private List<List<string>> mazes = new List<List<string>>
         {
-            // Set up form properties
-            this.Size = new Size(cellSize * mazeSize, cellSize * mazeSize);
-            this.DoubleBuffered = true;
-            this.Paint += new PaintEventHandler(DrawGameWindow);
-            this.KeyDown += new KeyEventHandler(HandleKeyDown);
-            this.PreviewKeyDown += (s, e) => { e.IsInputKey = true; };
-
-            List<string> maze = new List<string>
+            new List<string>
             {
                 "111111111111111",
                 "100000100100001",
@@ -46,62 +40,109 @@ namespace Northkorea_Run
                 "111011101111101",
                 "100000000000001",
                 "111111111111111"
-            };
+            },
+            new List<string>
+            {
+                "111111111111111",
+                "100000000100001",
+                "101111010101101",
+                "101000010001001",
+                "101011010101101",
+                "101010000000101",
+                "100011011110101",
+                "101000000010101",
+                "101111111000101",
+                "100000000010001",
+                "101110111110101",
+                "100010000000001",
+                "101111111111101",
+                "100000000000001",
+                "111111111111111"
+            },
+            new List<string>
+            {
+                "111111111111111",
+                "100000000000001",
+                "101111010111101",
+                "101000010000101",
+                "101011111110101",
+                "100000000010001",
+                "111011111010101",
+                "100010000000101",
+                "101111111110101",
+                "101000000000101",
+                "101110111110101",
+                "100000100000001",
+                "101110111110111",
+                "100000000000001",
+                "111111111111111"
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void InitializeComponent()
+        {
+            this.gameTimer = new Timer { Interval = 50 };
+            this.gameTimer.Tick += GameTimer_Tick;
+            this.Size = new Size(cellSize * mazeSize, cellSize * mazeSize);
+            this.DoubleBuffered = true;
+            this.Paint += DrawGameWindow;
+            this.KeyDown += HandleKeyDown;
+            this.PreviewKeyDown += (s, e) => e.IsInputKey = true;
+        }
+
+        private void StartGame()
+        {
             walls = new List<Rectangle>();
+            points = new List<Point>();
+            ghosts = new List<Ghost>();
+            Controls.Clear();
+
+            var maze = mazes[currentLevel];
+            var emptyCells = new List<Point>();
+
+            // Wände und leere Zellen ermitteln
             for (int y = 0; y < maze.Count; y++)
-            {
                 for (int x = 0; x < maze[y].Length; x++)
-                {
                     if (maze[y][x] == '1')
-                    {
                         walls.Add(new Rectangle(x * cellSize, y * cellSize, cellSize, cellSize));
-                    }
-                }
-            }
-            // Determine empty cells and spawn point
-            List<Point> emptyCells = new List<Point>();
-            for (int y = 0; y < maze.Count; y++)
-            {
-                for (int x = 0; x < maze[y].Length; x++)
-                {
-                    if (maze[y][x] == '0')
-                    {
+                    else
                         emptyCells.Add(new Point(x, y));
-                    }
-                }
-            }
-            Point spawnCell = emptyCells[0];
-            player = new Rectangle(spawnCell.X * cellSize + 5, spawnCell.Y * cellSize + 5, cellSize - 10, cellSize - 10);
-            playerDx = 0;
-            playerDy = 0;
-            wantedDx = 0;
-            wantedDy = 0;
-            // Place collectible points
-            Random rnd = new Random();
-            var freeForPoints = emptyCells.Where(p => !(p.X == spawnCell.X && p.Y == spawnCell.Y)).ToList();
-            var chosenPoints = new HashSet<int>();
-            while (chosenPoints.Count < 5 && chosenPoints.Count < freeForPoints.Count)
-            {
-                chosenPoints.Add(rnd.Next(freeForPoints.Count));
-            }
-            points = chosenPoints.Select(i => new Point(freeForPoints[i].X * cellSize, freeForPoints[i].Y * cellSize)).ToList();
+
+            // Spieler an erster leeren Zelle platzieren
+            var spawn = emptyCells[0];
+            player = new Rectangle(spawn.X * cellSize + 5,
+                                   spawn.Y * cellSize + 5,
+                                   cellSize - 10,
+                                   cellSize - 10);
+            playerDx = playerDy = wantedDx = wantedDy = 0;
+
+            // Sammelobjekte (5 Stück) zufällig verteilen
+            var rnd = new Random();
+            var free = emptyCells.Where(p => !p.Equals(spawn)).ToList();
+            var chosen = new HashSet<int>();
+            while (chosen.Count < 5 && chosen.Count < free.Count)
+                chosen.Add(rnd.Next(free.Count));
+            foreach (var i in chosen)
+                points.Add(new Point(free[i].X * cellSize, free[i].Y * cellSize));
+
+            // Ausgangstür setzen
             exitDoor = new Rectangle(13 * cellSize, 13 * cellSize, cellSize, cellSize);
             doorOpen = false;
 
-            // Initialize ghost(s)
-            ghosts = new List<Ghost>();
-            var forGhosts = emptyCells.Where(p => !(p.X == spawnCell.X && p.Y == spawnCell.Y) && !points.Any(pt => pt.X / cellSize == p.X && pt.Y / cellSize == p.Y)).ToList();
+            // Geister spawnen (bis zu 3)
             int ghostSize = cellSize - 10;
-            if (forGhosts.Count > 0)
+            for (int i = 0; i < Math.Min(3, free.Count); i++)
             {
-                Point ghostCell = forGhosts[forGhosts.Count - 1];
-                ghosts.Add(new Ghost(ghostCell.X * cellSize + 5, ghostCell.Y * cellSize + 5, ghostSize, 3));
+                var c = free[free.Count - 1 - i];
+                ghosts.Add(new Ghost(c.X * cellSize + 5, c.Y * cellSize + 5, ghostSize, 3));
             }
 
-            // Initialize game timer
-            gameTimer = new System.Windows.Forms.Timer();
-            gameTimer.Interval = 50;
-            gameTimer.Tick += new EventHandler(GameTimer_Tick);
             gameTimer.Start();
         }
 
@@ -109,106 +150,130 @@ namespace Northkorea_Run
         {
             switch (e.KeyCode)
             {
-                case Keys.Left:
-                    wantedDx = -1;
-                    wantedDy = 0;
-                    break;
-                case Keys.Right:
-                    wantedDx = 1;
-                    wantedDy = 0;
-                    break;
-                case Keys.Up:
-                    wantedDx = 0;
-                    wantedDy = -1;
-                    break;
-                case Keys.Down:
-                    wantedDx = 0;
-                    wantedDy = 1;
-                    break;
+                case Keys.Left: wantedDx = -1; wantedDy = 0; break;
+                case Keys.Right: wantedDx = 1; wantedDy = 0; break;
+                case Keys.Up: wantedDx = 0; wantedDy = -1; break;
+                case Keys.Down: wantedDx = 0; wantedDy = 1; break;
             }
         }
 
         private void GameTimer_Tick(object sender, EventArgs e)
         {
-            // Calculate wanted movement
-            Rectangle wantedPosition = player;
-            wantedPosition.X += wantedDx * playerSpeed;
-            wantedPosition.Y += wantedDy * playerSpeed;
-            if (!walls.Any(w => w.IntersectsWith(wantedPosition)))
-            {
-                playerDx = wantedDx;
-                playerDy = wantedDy;
-            }
-            // Calculate next position for actual movement
-            Rectangle nextPosition = player;
-            nextPosition.X += playerDx * playerSpeed;
-            nextPosition.Y += playerDy * playerSpeed;
-            if (!walls.Any(w => w.IntersectsWith(nextPosition)))
-            {
-                player = nextPosition;
-            }
-            // Remove collected points
-            int keySize = 32;
-            int offset = (cellSize - keySize) / 2;
-            points.RemoveAll(p => new Rectangle(p.X + offset, p.Y + offset, keySize, keySize).IntersectsWith(player));
+            // Neue Richtung übernehmen, wenn nicht blockiert
+            var wantPos = new Rectangle(player.X + wantedDx * playerSpeed,
+                                        player.Y + wantedDy * playerSpeed,
+                                        player.Width, player.Height);
+            if (!walls.Any(w => w.IntersectsWith(wantPos)))
+                (playerDx, playerDy) = (wantedDx, wantedDy);
+
+            // Spieler bewegen
+            var nextPos = new Rectangle(player.X + playerDx * playerSpeed,
+                                        player.Y + playerDy * playerSpeed,
+                                        player.Width, player.Height);
+            if (!walls.Any(w => w.IntersectsWith(nextPos)))
+                player = nextPos;
+
+            // Punkte einsammeln
+            int keySize = 32, offset = (cellSize - keySize) / 2;
+            points.RemoveAll(p => new Rectangle(p.X + offset, p.Y + offset, keySize, keySize)
+                                      .IntersectsWith(player));
+
+            // Ausgang öffnen, wenn alle Punkte weg sind
             if (!doorOpen && points.Count == 0)
-            {
                 doorOpen = true;
-            }
+
+            // Geister bewegen und Kollision prüfen
+            foreach (var gh in ghosts)
+                gh.MoveTowards(player, walls);
+            foreach (var gh in ghosts)
+                if (gh.Rect.IntersectsWith(player))
+                {
+                    gameTimer.Stop();
+                    MessageBox.Show("Game Over!");
+                    StartGame();
+                    return;
+                }
+
+            // Level abgeschlossen?
             if (doorOpen && exitDoor.IntersectsWith(player))
             {
                 gameTimer.Stop();
                 MessageBox.Show("Level completed!");
+                StartGame();
             }
-            if (ghosts != null)
-            {
-                foreach (var ghost in ghosts)
-                {
-                    ghost.MoveTowards(player, walls);
-                }
-            }
-            this.Invalidate();
+
+            Invalidate();
         }
 
         private void DrawGameWindow(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            // Clear background
+            var g = e.Graphics;
             g.Clear(Color.Black);
-            // Draw maze walls
-            foreach (Rectangle wall in walls)
+
+            // Wände zeichnen
+            foreach (var w in walls)
+                g.FillRectangle(Brushes.DarkRed, w);
+
+            // Punkte zeichnen
+            int keySize = 32, offset = (cellSize - keySize) / 2;
+            foreach (var p in points)
             {
-                g.FillRectangle(Brushes.DarkRed, wall);
+                g.FillEllipse(Brushes.Gold, new Rectangle(p.X + offset, p.Y + offset, keySize, keySize));
+                g.DrawEllipse(Pens.Gray, new Rectangle(p.X + offset, p.Y + offset, keySize, keySize));
             }
 
-            // Draw collectible points
-            int keySize = 32;
-            int offset = (cellSize - keySize) / 2;
-            foreach (var point in points)
-            {
-                g.FillEllipse(Brushes.Gold, new Rectangle(point.X + offset, point.Y + offset, keySize, keySize));
-                g.DrawEllipse(new Pen(Color.Gray, 2), new Rectangle(point.X + offset, point.Y + offset, keySize, keySize));
-            }
-
-            // Draw exit door if open
+            // Ausgang
             if (doorOpen)
             {
                 g.FillRectangle(Brushes.SaddleBrown, exitDoor);
-                g.DrawRectangle(new Pen(Color.Yellow, 4), exitDoor);
+                g.DrawRectangle(Pens.Yellow, exitDoor);
                 int knobR = 6;
-                g.FillEllipse(Brushes.Gold, exitDoor.X + exitDoor.Width - 14, exitDoor.Y + exitDoor.Height / 2 - knobR / 2, knobR, knobR);
+                g.FillEllipse(Brushes.Gold,
+                    exitDoor.X + exitDoor.Width - 14,
+                    exitDoor.Y + exitDoor.Height / 2 - knobR / 2,
+                    knobR, knobR);
             }
 
-            // Draw player as a gold circle
+            // Spieler
             g.FillEllipse(Brushes.Gold, player);
-            // Draw ghost(s)
-            if (ghosts != null)
+
+            // Geister als Flaggen
+            foreach (var gh in ghosts)
+                DrawGhostAsFlag(g, gh.Rect);
+        }
+
+        private void DrawGhostAsFlag(Graphics g, Rectangle rect)
+        {
+            g.FillEllipse(Brushes.Red, rect);
+            var star = StarPoints(
+                rect.X + rect.Width / 2,
+                rect.Y + rect.Height / 2,
+                rect.Width / 2.5f,
+                rect.Width / 5.5f,
+                5, -90
+            );
+            g.FillPolygon(Brushes.Gold, star);
+        }
+
+        private PointF[] StarPoints(
+            float cx, float cy,
+            float outerR, float innerR,
+            int numPoints, float startAngleDeg
+        )
+        {
+            var pts = new List<PointF>();
+            double angle = startAngleDeg * Math.PI / 180.0;
+            double step = Math.PI / numPoints;
+            for (int i = 0; i < numPoints * 2; i++)
             {
-                foreach (var ghost in ghosts)
-                {
-                    g.FillEllipse(Brushes.Red, ghost.Rect);
-                }
+                double r = (i % 2 == 0) ? outerR : innerR;
+                pts.Add(new PointF(
+                    cx + (float)(Math.Cos(angle) * r),
+                    cy + (float)(Math.Sin(angle) * r)
+                ));
+                angle += step;
             }
+            return pts.ToArray();
         }
 
         class Ghost
@@ -222,26 +287,34 @@ namespace Northkorea_Run
             }
             public void MoveTowards(Rectangle player, List<Rectangle> walls)
             {
-                int effectiveSpeed = speed;
-                List<Point> directions = new List<Point> { new Point(-1, 0), new Point(1, 0), new Point(0, -1), new Point(0, 1) };
+                int eff = speed;
+                var dirs = new[] { new Point(-1, 0), new Point(1, 0), new Point(0, -1), new Point(0, 1) };
                 double bestDist = double.MaxValue;
-                Point bestDir = new Point(0, 0);
-                foreach (Point dir in directions)
+                Point bestDir = new Point();
+                foreach (var d in dirs)
                 {
-                    Rectangle next = Rect;
-                    next.X += dir.X * effectiveSpeed;
-                    next.Y += dir.Y * effectiveSpeed;
+                    var next = Rect;
+                    next.X += d.X * eff;
+                    next.Y += d.Y * eff;
                     if (!walls.Any(w => w.IntersectsWith(next)))
                     {
-                        double dist = Math.Sqrt(Math.Pow(player.X - next.X, 2) + Math.Pow(player.Y - next.Y, 2));
+                        double dist = Math.Sqrt(
+                            Math.Pow(player.X - next.X, 2) +
+                            Math.Pow(player.Y - next.Y, 2)
+                        );
                         if (dist < bestDist)
                         {
                             bestDist = dist;
-                            bestDir = dir;
+                            bestDir = d;
                         }
                     }
                 }
-                Rect = new Rectangle(Rect.X + bestDir.X * effectiveSpeed, Rect.Y + bestDir.Y * effectiveSpeed, Rect.Width, Rect.Height);
+                Rect = new Rectangle(
+                    Rect.X + bestDir.X * eff,
+                    Rect.Y + bestDir.Y * eff,
+                    Rect.Width,
+                    Rect.Height
+                );
             }
         }
     }
